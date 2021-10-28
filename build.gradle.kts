281e8789df5868d3b5902ec09601634c82487336
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-10-27 23:28:13 -0300

    chore: project setup

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.5.6"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.31"
+	kotlin("plugin.spring") version "1.5.31"
+	kotlin("plugin.jpa") version "1.5.31"
+}
+
+group = "br.com.lukinhasssss"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2020.0.4"
+
+dependencies {
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+	implementation("org.flywaydb:flyway-core:8.0.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.5")
+	implementation("org.springframework.boot:spring-boot-starter-validation:2.5.5")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.5.5")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.0.4")
+
+	runtimeOnly("org.postgresql:postgresql:42.2.24.jre7")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus:1.7.5")
+
+	testImplementation("com.h2database:h2:1.4.200")
+	testImplementation("io.mockk:mockk:1.12.0")
+	testImplementation("io.rest-assured:kotlin-extensions:4.4.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/br/com/lukinhasssss/proposta/PropostaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.lukinhasssss.proposta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
