2f9b1e04dc1e0d769601adaaea337c51f172894d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-11-06 19:26:02 -0300

    fix: rename wrong words

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,22 +20,22 @@ extra["springCloudVersion"] = "2020.0.4"
 
 dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-	implementation("org.flywaydb:flyway-core:8.0.1")
+	implementation("org.flywaydb:flyway-core:8.0.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-validation:2.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-web:2.5.5")
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.6")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.6")
+	implementation("org.springframework.boot:spring-boot-starter-validation:2.5.6")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.5.6")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.0.5")
 
-	runtimeOnly("org.postgresql:postgresql:42.2.24.jre7")
+	runtimeOnly("org.postgresql:postgresql:42.3.1")
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus:1.7.5")
 
 	testImplementation("com.h2database:h2:1.4.200")
 	testImplementation("io.mockk:mockk:1.12.0")
 	testImplementation("io.rest-assured:kotlin-extensions:4.4.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
